f0718ae4f09f54fe69d93a21dc09187ad1970f43
Yang Chen <jackchen.chen@mail.utoronto.ca>
2024-11-20 13:58:01 -0500

    hw0 setup

---
+++ cmd.sh
@@ -0,0 +1,9 @@
+cmake -G "MinGW Makefiles" -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_FLAGS="-fexec-charset=UTF-8" ..
+make
+
+cmake -G "Visual Studio 17 2022" ..
+cmake --build .
+
+cmake --build . --config Release
+
+./build/Release/lajolla.exe ./scenes/cbox/cbox.xml
