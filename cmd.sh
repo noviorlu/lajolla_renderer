41c4c44d21f8963858f1ef94d79ed88677737768
Yang Chen <jackchen.chen@mail.utoronto.ca>
2025-01-16 18:31:58 -0500

    finished single scattering working on equiangular MIS

--- cmd.sh
+++ cmd.sh
@@ -21,3 +21,4 @@ msbuild ./build/lajolla.sln /p:Configuration=Release
 
 # HW2
 ./build/Release/lajolla.exe .\scenes\volpath_test\volpath_test1.xml
+./build/Release/lajolla.exe .\scenes\volpath_test\volpath_test2.xml
--- src/vol_path_tracing.h
+++ src/vol_path_tracing.h
@@ -39,6 +39,82 @@ Spectrum vol_path_tracing_2(const Scene &scene,
                             int x, int y, /* pixel coordinates */
                             pcg32_state &rng) {
     // Homework 2: implememt this!
+    int w = scene.camera.width, h = scene.camera.height;
+    Vector2 screen_pos((x + next_pcg32_real<Real>(rng)) / w,
+                       (y + next_pcg32_real<Real>(rng)) / h);
+    Ray ray = sample_primary(scene.camera, screen_pos);
+    RayDifferential ray_diff = RayDifferential{Real(0), Real(0)};
+    std::optional<PathVertex> vertex_ = intersect(scene, ray, ray_diff);
+    
+    Real t_hit = std::numeric_limits<Real>::infinity();
+    Medium medium = scene.media[scene.camera.medium_id];
+    PathVertex vertex;
+    if (vertex_) {
+        vertex = *vertex_;
+        int medium_id = dot(vertex.shading_frame.n, vertex.geometric_normal) < 0 ? vertex.interior_medium_id : vertex.exterior_medium_id;
+        medium = scene.media[medium_id];
+        t_hit = distance(vertex.position, ray.org);
+    }
+    
+    // since homogeneous medium, doesnot care about position
+    Spectrum sigma_a = get_sigma_a(medium, Vector3(0));
+    Spectrum sigma_s = get_sigma_s(medium, Vector3(0));
+    Spectrum sigma_t = sigma_a + sigma_s;
+
+    Real t = -log(1 - next_pcg32_real<Real>(rng)) / sigma_t[0];
+    if (t >= t_hit) {
+        // hit surface, account for emission
+        if(!is_light(scene.shapes[vertex.shape_id])) {
+            return make_zero_spectrum();
+        }
+        // where (transmittance / transmittance_pdf) 
+        //          = exp(-sigma_t * t_hit) / exp(-sigma_t * t_hit) = 1
+        return emission(vertex, -ray.dir, scene);
+    }
+    else{
+        // hit volume, account for single scattering
+        Vector3 volume_point = ray.org + t * ray.dir;
+
+        // sample a light source
+        Vector2 light_uv{next_pcg32_real<Real>(rng), next_pcg32_real<Real>(rng)};
+        Real light_w = next_pcg32_real<Real>(rng);
+        Real shape_w = next_pcg32_real<Real>(rng);
+        int light_id = sample_light(scene, light_w);
+        const Light &light = scene.lights[light_id];
+        PointAndNormal point_on_light =
+            sample_point_on_light(light, volume_point, light_uv, shape_w, scene);
+        
+        // compute G, p1, L, light_transmittance
+        Real t1 = distance(point_on_light.position, volume_point);
+        Vector3 dir_light = normalize(point_on_light.position - volume_point);
+        
+        Real G1 = 0;
+        Ray shadow_ray{volume_point, dir_light, get_shadow_epsilon(scene), t1 - get_shadow_epsilon(scene)};
+        if (!occluded(scene, shadow_ray))
+            G1 = max(abs(dot(dir_light, point_on_light.normal)), Real(0)) / (t1 * t1);
+
+        Real p1 = light_pmf(scene, light_id) *
+            pdf_point_on_light(light, point_on_light, volume_point, scene);
+
+        Spectrum L1 = Spectrum(0);
+        if(p1 > 0 && G1 > 0)
+            L1 = emission(light, -dir_light, Real(0), point_on_light, scene);
+
+        PhaseFunction& phase_function = get_phase_function(medium);
+        Spectrum rho1 = eval(phase_function, -ray.dir, dir_light);
+        
+        Spectrum light_transmittance1 = exp(-t1 * sigma_t);
+
+        Spectrum L_distanceSampling = rho1 * G1 * light_transmittance1 * L1 / p1;
+
+
+        // MIS with equiangular sampling
+        
+
+        // where (transmittance / transmittance_pdf) 
+        //      = exp(-sigma_t * t) / (exp(-sigma_t * t) * sigma_t) = 1 / sigma_t
+        return sigma_a / sigma_t *  L_distanceSampling;
+    }
     return make_zero_spectrum();
 }
 
